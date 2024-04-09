640f3e6c4023611d08a24570bf95855a8fec22de
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2024-04-09 15:04:44 -0500

    Update javagrep.js

--- javagrep.js
+++ javagrep.js
@@ -9,8 +9,10 @@ String().function("describe\ game\ aspect\");
                                      "\<[90]>/secprotocol/IMP.cpp",
                                     "\<[90]>/secbackbone/IMP.h", 
                                      "\<[90]>/secprotocol/IMP.h");
-mimeType[0].{catalog}::[
-  {String[ ""]},
+mimeType[0].{catalog()}
+catalog():
+[  for (catalog() == 0; catalog()>1; catalog()++;)
+{  {String[ ""]},
   {String['']},
 {String[ 0]},
   {char Str[""]},
@@ -18,5 +20,4 @@ mimeType[0].{catalog}::[
   {int String[""]},
 {double String[""]},
 {float String[""]},
-{Var string['']};
-];
+{Var string['']}  }  ]
