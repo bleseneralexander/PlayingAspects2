d5c239f4c7daa35af370be821322aebd121d89d2
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2024-04-09 15:02:42 -0500

    Create javagrep.js

---
+++ javagrep.js
@@ -0,0 +1,22 @@
+exec function GPMX();
+run prog mimeType[];
+String().function("describe\ game\ aspect\");
+                  mimeType[0].String("\<[90]>/secbackbone/controls.cpp",
+                                     "\<[90]>/secprotocol/controls.cpp",
+                                     "\<[90]>/secbackbone/gameView.cpp",
+                                    "\<[90]>/secprotocol/gameView.cpp",
+                                  "\<[90]>/secbackbone/IMP.cpp",
+                                     "\<[90]>/secprotocol/IMP.cpp",
+                                    "\<[90]>/secbackbone/IMP.h", 
+                                     "\<[90]>/secprotocol/IMP.h");
+mimeType[0].{catalog}::[
+  {String[ ""]},
+  {String['']},
+{String[ 0]},
+  {char Str[""]},
+  {static string[""]},
+  {int String[""]},
+{double String[""]},
+{float String[""]},
+{Var string['']};
+];
