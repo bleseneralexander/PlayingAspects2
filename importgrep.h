bd6f1acd03a430d58234db466b7ba1b768403387
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2025-08-21 20:56:01 -0500

    Update importgrep.h

--- importgrep.h
+++ importgrep.h
@@ -1,2 +1,7 @@
 import "importgrep.cpp";
-export "\PlayingAspects2/PlayingAspects/.&'{file}'";
+int main(importgrep);
+for (importgrep=0;i<250;i++)
+{/PlayingAspects2/PlayingAspects/.&'{file}';
+if (i=250)
+{i=0;}};
+export importgrep;
